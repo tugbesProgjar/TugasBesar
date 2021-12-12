6ed0742e9d1ac91ec7bf76fc358e01a30bcb825c
ellafk <ellafitrikusumaningrum@gmail.com>
2021-12-12 20:44:23 +0700

    2

--- src/tugasbesar/TugasBesar.java
+++ src/tugasbesar/TugasBesar.java
@@ -1,21 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tugasbesar;
+import java.io.Serializable;
+import javax.swing.ImageIcon;
+public class TugasBesar implements Serializable{
 
-/**
- *
- * @author Benny Irianto
- */
-public class TugasBesar {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+        public String getName() {
+        return name;
     }
-    
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public ImageIcon getImage() {
+        return image;
+    }
+
+    public void setImage(ImageIcon image) {
+        this.image = image;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public byte[] getFile() {
+        return file;
+    }
+
+    public void setFile(byte[] file) {
+        this.file = file;
+    }
+
+    private String status;
+    private ImageIcon image;
+    private byte[] file;
+    private String name;  
 }
